1bacf3c616ca6570bd338b680c32060bcd8d1252
Jessica Rodriguez <jessicarodriguez@Jessicas-MacBook-Pro.local>
2017-03-15 12:36:14 +0000

    Completed challenge 10, adding new instance of Bike class within DockingStation class

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,3 +3,15 @@ require 'docking_station'
 describe DockingStation do
   it { is_expected.to respond_to :release_bike} 
 end
+
+
+def initialize(release_bike)
+  @release_bike = release_bike	
+end
+
+# new instance of Bike Class
+Bike.new
+
+#release_bike has to return instance of Bike class
+station = Bike.new(release_bike)
+
