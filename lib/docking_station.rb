9f32814f1af9c19bb41fc0d7b5747b1aa7e1fc60
Jessica Rodriguez <jessicarodriguez@Jessicas-MacBook-Pro.local>
2017-03-16 17:32:54 +0000

    refactor constant and implement writer functionality

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,37 +2,40 @@ require_relative "bike"
 
 class DockingStation
 
-	DEFAULT_CAPACITY = 20
+  DEFAULT_CAPACITY = 20
 
-  attr_reader :stored_bikes
+  attr_accessor :capacity
 
-  def initialize
+  def initialize(capacity=DEFAULT_CAPACITY)
     @stored_bikes = []
+    @capacity = capacity
   end
 
 
   def release_bike
   	#guard condition raise - guards the rest of the code in this method, similar to if
     raise "No bikes available" if empty?
-    @stored_bikes.pop
+    stored_bikes.pop
   end
   
   # we pass argument bike, 
-  def dock(stored_bikes)
+  def dock(bike)
     raise "Docking station full" if full?
 
   	# creates an instance variable that stores the variable bike (instance of bike)
-  	@stored_bikes << stored_bikes
+  	stored_bikes << bike
   end
 
   private
 
+  attr_reader :stored_bikes
+
   def full?
-  	@stored_bikes.length >= DEFAULT_CAPACITY
-  end
+  	stored_bikes.length >= capacity
+  end  
 
   def empty?
-    @stored_bikes.empty?
+    stored_bikes.empty?
   end
 
 end
