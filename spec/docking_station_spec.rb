f407df3f09d73acbfab8ba5ccf96a60dab02d5d5
Jessica Rodriguez <jessicarodriguez@Jessicas-MacBook-Pro.local>
2017-03-16 11:55:04 +0000

    nested describe methods, added raise error expectation

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,17 +5,32 @@ describe DockingStation do
   it { is_expected.to respond_to(:dock).with(1).argument} 
   it { is_expected.to respond_to(:bike) }
 
-  it "releases working bikes" do
-    bike = subject.release_bike
-    expect(bike).to be_working
-  end
+  describe "#release_bike" do
+    it "releases a bike" do 
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike 
+    end
 
-  it "docks bike" do
-    bike = Bike.new
-    expect(subject.dock(bike)).to eq bike
+    it "releases working bikes" do
+      bike = Bike.new
+      subject.dock(bike)
+      subject.release_bike
+      expect(bike).to be_working
+    end
+
+    it "raises an error when no bikes available" do 
+      expect{ subject.release_bike }.to raise_error "No bikes available"  
+    end
+  end 
+  
+  describe '#dock' do
+    it "docks bike" do
+      bike = Bike.new
+      expect(subject.dock(bike)).to eq bike
+    end
   end
 
 end
-
 
 
