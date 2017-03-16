311f9ffda46c773a7d11eeff938251082a34a603
Jessica Rodriguez <jessicarodriguez@Jessicas-MacBook-Pro.local>
2017-03-16 15:00:34 +0000

    update tests to cater for increased bike capacity

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,13 +3,13 @@ require 'docking_station'
 describe DockingStation do
   it { is_expected.to respond_to :release_bike}
   it { is_expected.to respond_to(:dock).with(1).argument} 
-  it { is_expected.to respond_to(:bike) }
+  it { is_expected.to respond_to(:stored_bikes) }
 
   describe "#release_bike" do
     it "releases a bike" do 
       bike = Bike.new
       subject.dock(bike)
-      expect(subject.release_bike).to eq bike 
+      expect(subject.release_bike).to eq bike
     end
 
     it "releases working bikes" do
@@ -27,11 +27,11 @@ describe DockingStation do
   describe '#dock' do
     it "docks bike" do
       bike = Bike.new
-      expect(subject.dock(bike)).to eq bike
+      expect(subject.dock(bike)).to eq [bike]
     end
 
     it "raises an error when docking station is at full capacity" do
-      subject.dock(Bike.new)
+      20.times { subject.dock(Bike.new) }
       expect { subject.dock(Bike.new) }.to raise_error "Docking station full"
     end
   end
