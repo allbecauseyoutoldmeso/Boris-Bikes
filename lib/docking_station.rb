c8ed33b252460ad2517290f3808e3ddd193411dc
Jessica Rodriguez <jessicarodriguez@Jessicas-MacBook-Pro.local>
2017-03-16 11:53:55 +0000

    modified code to implement guard condition

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -4,11 +4,16 @@ class DockingStation
 
   attr_reader :bike
 
+
   def release_bike
-    Bike.new
+  	#guard condition raise - guards the rest of the code in this method, similar to if
+    raise "No bikes available" unless @bike 
+    @bike
   end
   
+  # we pass argument bike, 
   def dock(bike)
+  	# creates an instance variable that stores the variable bike (instance of bike)
   	@bike = bike
   end
 
